5ba5cae999cf41bbca4610645979afacf2c40099
Sarab <darsarab421@gmail.com>
2023-10-02 09:38:41 +0500

    Problem here Pages Not Showing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Layout from "./Layout";
-
+import {Routes,Route} from "react-router-dom";
+import ContactUsPage from "./pages/contactPage";
+import HomePage from "./pages/homePage";
+import ServicePage from "./pages/servicePage";
 function App() {
   return (
-    <Layout />
+ <>
+   <Routes>
+    <Route path="/" exact Component={HomePage}/>
+    <Route path="/service" exact Component={ServicePage}/>
+    <Route path="/contact-us" exact Component={ContactUsPage}/>
+   </Routes>
+ </>
   );
 }
 
---
+++ src/pages/servicePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Layout'
+
+const ServicePage = () => {
+  return (
+    <Layout>
+      <div> ServicePage</div>
+    </Layout>
+  )
+}
+
+export default ServicePage
